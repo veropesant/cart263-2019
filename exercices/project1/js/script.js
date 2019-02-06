aa3c8e0ae14b06a814748df4cc866b302f7f8577
veropesant <pesant.vero@gmail.com>
2019-02-06 17:41:21 -0500

    added some css styles and the cat animation (actually moving)

--- exercices/project1/js/script.js
+++ exercices/project1/js/script.js
@@ -15,20 +15,83 @@ author, and this description to match your project!
 // Description of preload
 let $kitty;
 let intv;
+let key;
+let isMoving;
+let keyR;
+let ev;
+let evR;
+let xCat;
 
 $(document).ready(function(){
 
   $kitty = $('#kitty');
+  $kitty.attr('src', 'assets/images/kitty.png');
+  xCat = 0;
 
-  intv = setInterval(walkKitty, 100);
 
 });
 
-function walkKitty(){
+$(function() {
+   $(window).keypress(function(e) {
+     //code found on stackoverflow
+       ev = e || window.event;
+       key = ev.keyCode || ev.which;
+       console.log(ev.type);
+       switch(key) {
+        case 13:
+          isMoving=true;
+          handleWalk();
+          break;
+        case 32:
+          console.log('eat');
+          break;
+        default:
+          console.log('default');
+      }
 
-  if($kitty.attr('src')==='assets/images/kitty1.png'){
-    $kitty.attr('src')='assets/images/kitty2.png';
-  }else{
-    $kitty.attr('src')='assets/images/kitty1.png';
+   });
+
+   $(window).keyup(function(e){
+     evR = e || window.event;
+     keyR = evR.keyCode || evR.which;
+     console.log(keyR);
+     if(keyR==13){
+       isMoving=false;
+       handleWalk();
+     }
+   });
+});
+
+function handleWalk(){
+  if(isMoving==true){
+    if(!intv){
+      intv = setInterval(walkKitty, 200);
+
+    }
   }
+  else{
+    clearInterval(intv);
+    intv = null;
+    walkKitty();
+
+  }
+}
+
+function walkKitty(){
+  if(isMoving==true){
+    xCat = xCat+5;
+  }
+
+  console.log($kitty);
+  $kitty.css('left', xCat);
+  if(isMoving==true){
+    if($kitty.attr('src')==='assets/images/kitty1.png'){
+      $kitty.attr('src','assets/images/kitty2.png');
+    }else{
+      $kitty.attr('src','assets/images/kitty1.png');
+    }
+  }else{
+    $kitty.attr('src', 'assets/images/kitty.png');
+  }
+
 }
