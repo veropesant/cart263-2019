dc849c8f95cd375b3af0be7083b5a9a0e4571868
Véronique Pesant <pesant.vero@gmail.com>
2019-02-28 22:25:39 -0500

    added a function to display the score and some instructions at the beginning

--- exercices/assignment5/js/script.js
+++ exercices/assignment5/js/script.js
@@ -8,8 +8,9 @@ author, and this description to match your project!
 
 ******************/
 let answers;
+let correctAnswers=0;
 let NUM_OPTIONS = 5;
-let animals = [
+let animals = [ // array of all the possible animals to use in the game
       "aardvark",
       "alligator",
       "alpaca",
@@ -153,16 +154,23 @@ let options={
 };
 
 $(document).ready(function(){
+  $('#score').html("Click on the right answer or say I THINK IT'S followed by your guess<br>Say SAY IT AGAIN to repeat the word to guess<br>Say I GIVE UP if you can't find the word");
   $('#click-here').on('click', function(){
     this.remove();
+    displayScore();
     startGame();
   });
   if (annyang) {
     // Let's define our first command. First the text we expect, and then the function it should call
     let commands = {
       'I give up': function() {
+        //sets the number of correct answers to 0
+        correctAnswers=0;
+        displayScore();
+        //finds the index of the animal in the array since it's the same id in the html
         let idAnimal = answers.indexOf(correctAnimal);
         $('#'+idAnimal).effect('shake');
+        //after 2 secs, new round, to let time for the shake effect
         setTimeout(function(){
           $('.guess').remove();
           newRound();
@@ -177,9 +185,17 @@ $(document).ready(function(){
         if(tag==correctAnimal){
           $('.guess').remove();
           setTimeout(newRound,2000);
+          //adds 1 to the current score
+          correctAnswers+=1;
+          displayScore();
+
         }
         else{
           speakAnimal(correctAnimal);
+          //sets the number of correct answers to 0
+          correctAnswers=0;
+          displayScore();
+
         }
       }
     };
@@ -197,36 +213,53 @@ function startGame(){
   newRound();
 }
 
+//creates a new button in the html for each possible answer
 function addButton(label, id){
   let $guess = $("<div class='guess' id='"+id+"'></div>");
   $guess.html(label);
   $guess.button();
   $guess.on('click', function(){
-
+    //checks the clicked guesses
     if($guess.html()==correctAnimal){
+      //adds 1 to the current score
+      correctAnswers+=1;
+      displayScore();
       $('.guess').remove();
       setTimeout(newRound,2000);
     }else{
       $(this).effect('shake');
       speakAnimal(correctAnimal);
+      //sets the number of correct answers to 0
+      correctAnswers=0;
+      displayScore();
+
     }
   });
+  //adds the buttons to the body
   $('body').append($guess);
 }
-
+//function that picks 6 new animals to display as possible answers
 function newRound(){
   answers = [];
   for(i=0; i <= NUM_OPTIONS; i++){
+    //random animal in the complete array of animals
     let animal = animals[Math.floor(Math.random()*animals.length)];
+    //calls the function to create a new button with the random animal
     addButton(animal, i);
     answers.push(animal);
   }
+  //selects the correct animal randomly in the array of answers and calls the function to speak it
   correctAnimal = answers[Math.floor(Math.random()*answers.length)];
   speakAnimal(correctAnimal);
 }
 
+//voice says the animal to find, backwards
 function speakAnimal(animal){
   let reverseAnimal = animal.split('').reverse().join('');
   responsiveVoice.speak(reverseAnimal,"UK English Male" , options);
 
+}
+//displays the current score
+function displayScore(){
+  $('#score').html('You guessed '+correctAnswers+' animals right!');
 }
