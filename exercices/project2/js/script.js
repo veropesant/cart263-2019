a8dfeb4c4bf8d758ac9c0e7970607fb5f7a5333e
veropesant <pesant.vero@gmail.com>
2019-03-13 16:31:29 -0400

    trying another word

--- exercices/project2/js/script.js
+++ exercices/project2/js/script.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
     // Let's define our first command. First the text we expect, and then the function it should call
     var commands = {
       //when you say fuck, the dino can jump
-      'fuck': function() {
+      'shit': function() {
         jump();
       }
     };
